e898d87d1b12f0ccea4ec88a0f35f08e69de351d
Elvira <gaellru@gmail.com>
2024-02-26 01:28:26 +0300

    fix pagination

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -3,86 +3,93 @@ import { ProductItem } from "./ProductItem";
 import { baseApi } from "../../store/services";
 import s from "./styles.module.scss"
 import { Filters } from "../filters/Filters";
-import { useSelector } from "react-redux";
-import { filter } from "../../store/filterSlice";
-import { Pagination } from "antd";
-
-export const limit = 50
+import { useDispatch, useSelector } from "react-redux";
+import { filter, toggleIsFilter } from "../../store/filterSlice";
+import { Button, Pagination } from "antd";
+import { LIMIT } from "../../constants/constants";
 
 export function ProductList() {
-  const [getIdsCount, { data: idsCount }] = baseApi.useGetIdsCountMutation()
-  const [getIds, { data: idsData }] = baseApi.useGetIdsMutation()
-  const [getItems, { data: products }] = baseApi.useGetItemsMutation()
-  const [_, { data: filteredIds }] = baseApi.useFilterMutation({fixedCacheKey: 'sharedFilter'})
-  const { isFilter } = useSelector(filter)
+    const [isFilterVisible, setIsFilterVisible] = useState(false)
+    const [getIdsCount, { data: idsCount }] = baseApi.useGetIdsCountMutation()
+    const [getIds, { data: idsData }] = baseApi.useGetIdsMutation()
+    const [getItems, { data: products }] = baseApi.useGetItemsMutation()
+    const [_, { data: filteredIds }] = baseApi.useFilterMutation({fixedCacheKey: 'sharedFilter'})
+    const { isFilter } = useSelector(filter)
+    const dispatch = useDispatch()
 
-
-  const offsetRef = useRef(0)
-  const [isBack, setIsBack] = useState(false)
+    const offsetRef = useRef(0)
+    const [isBack, setIsBack] = useState(false)
 
 //   console.log('isBack', isBack)
 
-  const getProducts = (idsData) => {
-    if (idsData) {
-        if (idsData.ids.length < limit && !idsData.isMaxLimit) {
-            if (isBack) {
-                getIds({offset: offsetRef.current - (limit - idsData.ids.length), limit: limit + (limit - idsData.ids.length)})
-                getItems(idsData.ids)
+    const getProducts = (idsData) => {
+        if (idsData) {
+            if (idsData.ids.length < LIMIT && !idsData.isMaxLimit) {
+                if (isBack) {
+                    getIds({offset: offsetRef.current - (LIMIT - idsData.ids.length), limit: LIMIT + (LIMIT - idsData.ids.length)})
+                    getItems(idsData.ids)
+                } else {
+                    getIds({offset: offsetRef.current, limit: LIMIT + (LIMIT - idsData.ids.length)})
+                    getItems(idsData.ids)
+                    offsetRef.current += LIMIT - idsData.ids.length
+                }
             } else {
-                getIds({offset: offsetRef.current, limit: limit + (limit - idsData.ids.length)})
+                //TODO - если передаются фильрованные айдишники и их больше лимита, то нужно их передавать порциями
                 getItems(idsData.ids)
-                offsetRef.current += limit - idsData.ids.length
             }
-        } else {
-            getItems(idsData.ids)
         }
-    //
     }
-  }
 
+    useEffect(() => {
+        getIdsCount()
+        getIds({offset: offsetRef.current, limit: LIMIT})
+    }, [])
 
-  useEffect(() => {
-    getIdsCount()
-    getIds({offset: offsetRef.current, limit})
-  }, [])
+    useEffect(() => {
+        getProducts(isFilter ? filteredIds : idsData)
+    }, [isFilter ? filteredIds : idsData])
 
-  useEffect(() => {
-    getProducts(isFilter ? filteredIds : idsData)
-  }, [isFilter ? filteredIds : idsData])
+    //   console.log('offsetRef.current', offsetRef.current)
 
-//   console.log('offsetRef.current', offsetRef.current)
+    const handlePageChange = (page, pageSize) => {
+        const newOffset = (page - 1) * pageSize + (offsetRef.current % pageSize)  // Вычисляем новое смещение
+        getIds({ offset: newOffset, limit: LIMIT })
+        if (newOffset < offsetRef.current) {
+            // Движение назад
+            setIsBack(true)
+        } else {
+            // Движение вперёд или на ту же страницу
+            setIsBack(false)
+        }
+        offsetRef.current = newOffset // Обновляем значение смещения в ref
+    }
 
-  const handlePageChange = (page, pageSize) => {
-    const newOffset = (page - 1) * pageSize + (offsetRef.current % pageSize)  // Вычисляем новое смещение
-    getIds({ offset: newOffset, limit }) // Вызываем вашу функцию для загрузки данных
-    if (newOffset < offsetRef.current) {
-        // Движение назад
-        setIsBack(true)
-      } else {
-        // Движение вперёд или на ту же страницу
-        setIsBack(false)
-      }
-    offsetRef.current = newOffset // Обновляем значение смещения в ref
-  }
+    const toggleFilterVisible = () => {
+        setIsFilterVisible(!isFilterVisible)
+        if (isFilterVisible) {
+            dispatch(toggleIsFilter(false))
+        }
+    }
 
-  return (
-    <div className={s.wrapper}>
-        <div className={s.list}>
-        {products && products.map(item => <ProductItem key={item.id} productData={item}/>)}
+    return (
+        <div className={s.wrapper}>
+            <div className={s.filter}>
+                <Button onClick={toggleFilterVisible}>Фильтр</Button>
+                {isFilterVisible && <Filters/>}
+            </div>
+            <div className={s.list}>
+                {products && products.map(item => <ProductItem key={item.id} productData={item}/>)}
+            </div>
+            <div className={s.pagination}>
+                <Pagination
+                defaultPageSize={LIMIT}
+                total={isFilter ? filteredIds?.ids?.length : idsCount}
+                showSizeChanger={false}
+                showTotal={(total, range) => `${range[0]}-${range[1]} из ${total} товаров`}
+                onChange={handlePageChange}
+                />
+            </div>
         </div>
-        <div className={s.pagination}>
-            <Pagination
-            defaultPageSize={limit}
-            total={idsCount}
-            showSizeChanger={false}
-            showTotal={(total, range) => `${range[0]}-${range[1]} из ${total} товаров`}
-            onChange={handlePageChange}
-            />
-            {/* <button className={s.btn} onClick={goToPreviousPage} disabled={offsetRef.current < limit}>Назад</button>
-            <button className={s.btn} onClick={goToNextPage} disabled={products?.length < limit}>Вперед</button> */}
-        </div>
-        <Filters/>
-    </div>
-  );
+    )
 }
 
