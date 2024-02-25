6705bb6cd2576a10de4ec6d9d23a1678fb8ccdb0
Elvira <gaellru@gmail.com>
2024-02-26 01:28:09 +0300

    add form validate

---
+++ src/components/ui/SubmitButton.jsx
@@ -0,0 +1,23 @@
+import { Button, Form } from 'antd';
+import { useEffect, useState } from 'react';
+export const SubmitButton = ({ form, children }) => {
+    const [submittable, setSubmittable] = useState(false);
+
+    // Watch all values
+    const values = Form.useWatch([], form)
+
+    useEffect(() => {
+    form
+        .validateFields({
+        validateOnly: true,
+        })
+        .then(() => setSubmittable(true))
+        .catch(() => setSubmittable(false))
+    }, [form, values])
+
+    return (
+        <Button htmlType="submit" disabled={!submittable}>
+        {children}
+        </Button>
+    )
+}
