95528ac4d1986cd5571e1c3a34942ee30d5dec7a
Elvira <gaellru@gmail.com>
2024-03-02 01:14:55 +0300

    add theme and global variables

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import { store } from './store/store';
+import { ConfigProvider } from 'antd';
+import { theme } from './theme/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
+    <ConfigProvider theme={theme}>
       <App />
+    </ConfigProvider>
     </Provider>
 );
